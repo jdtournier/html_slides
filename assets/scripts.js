93f529e1c6902aca39d3d5c5d4ad304d4755101d
J-Donald Tournier <jdournier@gmail.com>
2026-08-12 23:49:01 +0100

    switch to markdown converter with ability to set attributes

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,6 @@
 "use strict";
 
-//const hljs = require('highlight.js/lib/core');
-
-// Load any languages you need
-//hljs.registerLanguage('javascript', require('highlight.js/lib/languages/c++'));
-//hljs.registerLanguage('javascript', require('highlight.js/lib/languages/bash'));
-
-
-var full_screen = function full_screen(element) {
+function full_screen(element) {
   if (element.requestFullscreen) {
     element.requestFullscreen();
   } else if (element.mozRequestFullScreen) {
@@ -21,53 +14,50 @@ var full_screen = function full_screen(element) {
 
 var slide_count = document.querySelectorAll('section').length;
 
-var ensure_visible = function ensure_visible () {
-  document.getElementById (window.location.hash.slice(1)).style.visibility = 'visible';
-}
-
-var slide_number = function slide_number() {
+function slide_number() {
   var activeSlide = document.querySelector('[id^="slide"]:target');
   if (!activeSlide) { return 0; }
-  return parseInt(activeSlide.getAttribute('id').substring(5));
+  return parseFloat(activeSlide.getAttribute('id').substring(5));
 };
 
-var is_last_slide = function is_last_slide () {
+function is_last_slide () {
   return window.location.hash === '#slide' + (slide_count - 1);
 };
 
-var is_first_slide = function is_first_slide () {
+function is_first_slide () {
   return window.location.hash === '#slide0';
 };
 
-var first_slide = function first_slide() {
+function first_slide() {
   window.location.hash = '#slide0';
-  ensure_visible();
 }
 
-var next_slide = function next_slide() {
-  if (is_last_slide()) { return; }
-  window.location.hash = '#slide' + (slide_number() + 1);
-  ensure_visible();
+function next_slide() {
+  const slides = Array.from(document.querySelectorAll('[id^="slide"]'));
+  const current = slides.findIndex(slide => `#${slide.id}` === window.location.hash);
+  if (current < slides.length-1)
+    window.location.hash = '#' + slides[current+1].id;
 };
 
-var prev_slide = function prev_slide() {
-  if (is_first_slide()) { return; }
-  window.location.hash = '#slide' + (slide_number() - 1);
-  ensure_visible();
+function prev_slide() {
+  const slides = Array.from(document.querySelectorAll('[id^="slide"]'));
+  const current = slides.findIndex(slide => `#${slide.id}` === window.location.hash);
+  if (current > 0)
+    window.location.hash = '#' + slides[current-1].id;
 };
 
-var on_scroll = function on_scroll(event) {
+function on_scroll(event) {
   if (event.ctrlKey || event.shiftKey || event.altKey || event.metaKey) { return; }
   if (event.deltaY > 0) { next_slide(); }
   else if (event.deltaY < 0) { prev_slide(); }
 };
 
 
-var process_key = function process_key(event) {
-  if (event.code === 'Space' || event.code === 'ArrowRight' || event.code === 'PageDown') {
+function process_key(event) {
+  if (event.code === 'Space' || event.code === 'ArrowRight' || event.code === 'PageDown' || event.code === 'ArrowDown') {
     next_slide();
   }
-  else if (event.code === 'ArrowLeft' || event.code === 'PageUp') {
+  else if (event.code === 'ArrowLeft' || event.code === 'PageUp' || event.code === 'ArrowUp') {
     prev_slide();
   }
   else if (event.code === 'KeyR') {
@@ -79,13 +69,6 @@ var process_key = function process_key(event) {
 };
 
 
-const sections = document.querySelectorAll('section');
-sections.forEach((section, index) => {
-  section.style.visibility = 'hidden';
-  section.id = `slide${index}`;
-  if (index > 0) { section.innerHTML += `<div class=slidenumber><p>${index}</p></div>`; }
-});
-
 document.body.addEventListener('keydown', process_key, false);
 document.addEventListener('click', next_slide, false);
 document.addEventListener('wheel', on_scroll, false);
@@ -93,5 +76,5 @@ document.addEventListener('wheel', on_scroll, false);
 if (!window.location.hash) {
   first_slide();
 }
-ensure_visible();
+//ensure_visible();
 
---
+++ slides.md
@@ -0,0 +1,57 @@
+Intro to C++
+Slide to get started in C++
+
+# Intro to C++
+## Session 1A
+### 5CCYB041 - Introduction to C++
+
+---
+
+# First slide
+
+@(right: 10%; bottom: 10%)
+Here we talk about *something*, and show **some code**
+
+--
+
+```
+#include <iostream>
+
+int main () 
+{
+  std::cout << "Hello World\n";
+  return 0;
+}
+```
+
+---
+
+#Second slide
+
+
+Here we talk about something else
+
+
+@code(left: 20%; top: 30%)
+```
+#include <iostream>
+
+int main (int argc, char* argv[]) 
+{
+  std::cout << "Hello World: " << argv[1] << "\n";
+  return 0;
+}
+```
+--
+@note(right:10%; top: 20%)
+> We can print to the terminal using \\
+> the `std::cout` **output 
+> stream**
+
+---
+
+# Meet the team
+
+@donald.col(width: 50%; height: auto)
+![Donald's mug](assets/donald.jpg)
+
